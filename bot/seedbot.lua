5b1bf9c51456d9655f7c34630367732debab2b66
neghabdar <3919779@gmail.com>
2017-05-05 19:02:56 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {228018251,0,tonumber(our_id)},--Sudo users
+    sudo_users = {228018251},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
