981f229538c23d5fa6b87d5d6c16abe870b82124
neghabdar <3919779@gmail.com>
2017-05-05 22:25:11 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,7 +232,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {228018251},--Sudo users
+    sudo_users = {25244535},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
